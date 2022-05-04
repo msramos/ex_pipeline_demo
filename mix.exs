9fc17a54c1f663e6ee94bb597c65b31c658da496
Marcos Ramos <rms.mrcs@gmail.com>
2022-05-04 02:33:40 -0300

    Add ex_pipeline

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Blog.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_pipeline, github: "msramos/ex_pipeline"}
     ]
   end
 
